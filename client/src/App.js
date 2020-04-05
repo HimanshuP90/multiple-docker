381dd83a68dee3d3497e709631269d8c6a56e9bd
HimanshuP90 <himanshup895@gmail.com>
2020-04-05 12:55:19 +0530

    Multiple Images initial setup

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+            <h1 className="App-title">Fib Calculator</h1>
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">Other Page</Link>
+            <hr /> <br />
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
